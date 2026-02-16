353e12a95f23178bd169d2dc2f39bf4846e081e8
dipan0saha <dipan.saha@gmail.com>
2026-02-16 08:43:30 -0500

    Misc changes

--- test/features/gallery/presentation/providers/gallery_screen_provider_test.dart
+++ test/features/gallery/presentation/providers/gallery_screen_provider_test.dart
@@ -47,7 +47,7 @@ void main() {
         table: anyNamed('table'),
         channelName: anyNamed('channelName'),
         filter: anyNamed('filter'),
-      )).thenAnswer((_) => Stream.empty());
+      )).thenAnswer((_) => Stream.value(<String, dynamic>{}));
       when(mockRealtimeService.unsubscribe(any)).thenAnswer((_) async {});
 
       // Create container AFTER all default mocks are setup
--- test/tiles/recent_purchases/providers/recent_purchases_provider_test.dart
+++ test/tiles/recent_purchases/providers/recent_purchases_provider_test.dart
@@ -36,7 +36,7 @@ void main() {
         table: anyNamed('table'),
         channelName: anyNamed('channelName'),
         filter: anyNamed('filter'),
-      )).thenAnswer((_) => Stream.empty());
+      )).thenAnswer((_) => Stream.value(<String, dynamic>{}));
       when(mocks.realtime.unsubscribe(any)).thenAnswer((_) async {});
 
       container = ProviderContainer(
